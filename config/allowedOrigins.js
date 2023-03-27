d825e65ea2af30e402e55669045d70a8c22ac822
Darwin Facelo <darwinfacelo@yahoo.com>
2023-03-27 13:07:11 -0700

    added cors option for site

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:8000",
   "http://localhost:3000",
+  "https://linked-in-front-end-production.up.railway.app"
 ];
 
 export default allowedOrigins;
